c4ce2cfcead7307497cec7c35ff30f77f1d2f552
guemplen <57450955+guemplen@users.noreply.github.com>
2023-08-25 18:23:31 +0000

    Deploying to gh-pages from @ guemplen/goit-js-hw-06@27d0662249350b186e2582687ea14a1ce568a4c7 🚀

---
+++ task-01.09957ee7.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelectorAll(".item");console.log(`Number of categories: ${e.length}`),e.forEach((e=>{const o=e.querySelector("h2").textContent,l=e.querySelectorAll("ul li");console.log(`Category: ${o}`),console.log(`Elements: ${l.length}`)}))}();
+//# sourceMappingURL=task-01.09957ee7.js.map
---
+++ task-01.afe9efbd.js
@@ -0,0 +1,2 @@
+!function(){var e;e=document.querySelectorAll(".item"),console.log("Number of categories: ".concat(e.length)),e.forEach((function(e){var o=e.querySelector("h2").textContent,t=e.querySelectorAll("ul li");console.log("Category: ".concat(o)),console.log("Elements: ".concat(t.length))}))}();
+//# sourceMappingURL=task-01.afe9efbd.js.map
