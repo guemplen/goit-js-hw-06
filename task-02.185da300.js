d5d86054a471965428b87aadfabfce4934175437
guemplen <57450955+guemplen@users.noreply.github.com>
2023-08-25 21:32:01 +0000

    Deploying to gh-pages from @ guemplen/goit-js-hw-06@0206c3a82de409350de06673e0994925ae291239 🚀

---
+++ task-02.185da300.js
@@ -0,0 +1,2 @@
+!function(e){const t=document.getElementById("ingredients");e.forEach((e=>{const n=document.createElement("li");n.className="item",n.textContent=e,t.append(n)}))}(["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"]);
+//# sourceMappingURL=task-02.185da300.js.map
---
+++ task-02.b1b677f5.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"],t=document.getElementById("ingredients"),e.forEach((function(e){var n=document.createElement("li");n.className="item",n.textContent=e,t.append(n)}))}();
+//# sourceMappingURL=task-02.b1b677f5.js.map
