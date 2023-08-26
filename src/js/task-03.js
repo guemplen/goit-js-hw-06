a4f5654dcb610e910c0980bbdd5575a6e6cc2234
guemplen <guemplen@gmail.com>
2023-08-26 12:01:52 +0200

    Made task3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,40 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// const imagesUl = document.querySelector('.gallery');
+// imagesUl.className = 'gallery-item';
+
+// const makeGallery = imagesInfo => {
+//   const { url, alt } = imagesInfo;
+//   const addList = document.createElement('li');
+//   const addImg = document.createElement('img');
+//   addImg.src = url;
+//   addImg.alt = alt;
+//   addImg.width = '400';
+//   addList.append(addImg);
+//   imagesUl.append(addList);
+// };
+
+// const galleryImages = images.map(imagesInfo => makeGallery(imagesInfo));
+
+const imagesUl = document.querySelector('.gallery');
+imagesUl.className = 'gallery-item';
+
+const makeGallery = imagesInfo => {
+  const { url, alt } = imagesInfo;
+  return `
+    <li>
+        <img src="${url}" alt="${alt}" width="400">
+    </li>
+  `;
+};
+
+const galleryImages = images.map(imagesInfo => makeGallery(imagesInfo));
+imagesUl.insertAdjacentHTML('beforeend', galleryImages.join(''));
+
+//Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
+//Используй массив объектов images для создания элементов <img> вложенных в <li>.
+//Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+//Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+//Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
