6828e020ef9b0102c9db6b5e9ccea14b68f9a13f
guemplen <57450955+guemplen@users.noreply.github.com>
2023-08-27 21:10:31 +0000

    Deploying to gh-pages from @ guemplen/goit-js-hw-06@ac010257f12de9680a69b1059c6da81a49d826c8 🚀

---
+++ task-06.9848506f.js
@@ -0,0 +1,2 @@
+const t=document.getElementById("validation-input"),a=parseInt(t.getAttribute("data-length"));t.addEventListener("blur",(function(){t.value.length===a?(t.classList.add("valid"),t.classList.remove("invalid")):(t.classList.add("invalid"),t.classList.remove("valid"))}));
+//# sourceMappingURL=task-06.9848506f.js.map
