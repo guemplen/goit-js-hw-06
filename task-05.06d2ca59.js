2ba740c4388b65b62be4579d27127c191f52ba19
guemplen <57450955+guemplen@users.noreply.github.com>
2023-08-27 19:37:40 +0000

    Deploying to gh-pages from @ guemplen/goit-js-hw-06@a32698829717cd760e0c1adb1ab931cb125398ba 🚀

---
+++ task-05.06d2ca59.js
@@ -0,0 +1,2 @@
+!function(){var t=document.getElementById("name-input"),n=document.getElementById("name-output");t.addEventListener("input",(function(){var e=t.value;n.textContent=e,""===t.value&&(n.textContent="Anonymous")}))}();
+//# sourceMappingURL=task-05.06d2ca59.js.map
---
+++ task-05.b70de88e.js
@@ -0,0 +1,2 @@
+const t=document.getElementById("name-input"),n=document.getElementById("name-output");t.addEventListener("input",(function(){const e=t.value;n.textContent=e,""===t.value&&(n.textContent="Anonymous")}));
+//# sourceMappingURL=task-05.b70de88e.js.map
